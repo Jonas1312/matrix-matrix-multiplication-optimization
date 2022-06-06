abdbfafeb918e0b359d2d115fab2dfa781e72d18
Jonas Maison <Jonas1312@users.noreply.github.com>
2022-06-06 13:31:18 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,98 @@
+#include <cassert>
+#include <chrono>
+#include <iostream>
+#include <vector>
+
+#define N 1024
+
+using matrix = std::vector<std::vector<double>>;
+
+void reset_matrix(matrix &C) {
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < N; ++j) {
+            C[i][j] = 0.0;
+        }
+    }
+}
+
+bool are_equal(const matrix &X, const matrix &Y) {
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < N; ++j) {
+            if (X[i][j] != Y[i][j]) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+auto multiply_ijk(const matrix &A, const matrix &B, matrix &C) {
+    auto start = std::chrono::high_resolution_clock::now();
+
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < N; ++j) {
+            for (int k = 0; k < N; k++) {
+                C[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
+
+    auto end = std::chrono::high_resolution_clock::now();
+
+    return std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+}
+
+auto multiply_ikj(const matrix &A, const matrix &B, matrix &C) {
+    auto start = std::chrono::high_resolution_clock::now();
+
+    for (int i = 0; i < N; ++i) {
+        for (int k = 0; k < N; k++) {
+            for (int j = 0; j < N; ++j) {
+                C[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
+
+    auto end = std::chrono::high_resolution_clock::now();
+
+    return std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+}
+
+int main(int argc, char **argv) {
+    matrix A, B, C_ijk, C_ikj;
+
+    // Initialize sizes
+    A.resize(N);
+    B.resize(N);
+    C_ijk.resize(N);
+    C_ikj.resize(N);
+    for (int i = 0; i < N; ++i) {
+        A[i].resize(N);
+        B[i].resize(N);
+        C_ijk[i].resize(N);
+        C_ikj[i].resize(N);
+    }
+
+    // Initialize A and B values
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < N; ++j) {
+            A[i][j] = static_cast<double>(i * N + j);
+            B[i][j] = static_cast<double>(i * N + j);
+        }
+    }
+
+    // BENCHMARK
+    for (int s = 0; s < 5; ++s) {
+        std::cout << std::endl << "Step: " << s << std::endl;
+
+        reset_matrix(C_ijk);
+        std::cout << "IJK: " << multiply_ijk(A, B, C_ijk) << " ms" << std::endl;
+
+        reset_matrix(C_ikj);
+        std::cout << "IKJ: " << multiply_ikj(A, B, C_ikj) << " ms" << std::endl;
+
+        assert(are_equal(C_ijk, C_ikj));
+    }
+
+    return 0;
+}
